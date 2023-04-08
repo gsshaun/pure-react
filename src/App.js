2a98ba5fa9178c1f96d58e9cf9db9ce4988e0b5c
Golam Sarwar <gsshaun@gmail.com>
2023-04-08 21:04:21 +0900

    pass information with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.bread),
+    ]);
+};
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me")
+        [
+            React.createElement("h1", {}, "Adopt Me"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Luna",
+                bread: "Havanese"
+            }),
+                React.createElement(Pet, {
+                animal: "Bird",
+                name: "Pepet",
+                bread: "Cocktiel"
+            }),
+                React.createElement(Pet, {
+                animal: "Cat",
+                name: "Lilu",
+                bread: "British"
+            }),
+        ]
     )
 };
 const container = document.getElementById('root');
