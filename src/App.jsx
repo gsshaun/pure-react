f212e7d7b5bc02a2e122a98cfdbcf24f63547ba4
Golam Sarwar <gsshaun@gmail.com>
2023-04-09 11:15:32 +0900

    implement es-lint

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,6 @@ import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me"),
-  //   React.createElement(Pet, {
-  //     animal: "Dog",
-  //     name: "Luna",
-  //     bread: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     animal: "Bird",
-  //     name: "Pepet",
-  //     bread: "Cocktiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     animal: "Cat",
-  //     name: "Lilu",
-  //     bread: "British",
-  //   }),
-  // ]);
-
   <div>
     <h1>Adopt Me!</h1>
     <Pet name="Luna" animal="Dog" breed="Havanese" />
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,22 +1,11 @@
-// import React from "react";
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.bread),
-//     ]);
-//   };
-
 const Pet = (props) => {
-    return(
-      <div>
-        <h1>{props.name}</h1>
-        <h2>{props.animal}</h2>
-        <h3>{props.breed}</h3>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h3>{props.breed}</h3>
+    </div>
+  );
+};
 
-
-  export default Pet;
+export default Pet;
